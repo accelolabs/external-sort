8d2b86c135235e291bb58c034b64894aeb536d1f
accelolabs <accelotron@niuitmo.ru>
2025-05-03 03:53:16 +0300

    feat: tape interface

---
+++ lib/ITape.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+template<typename T>
+class ITape {
+
+public:
+    
+    virtual T read() const = 0;
+    virtual void write(const T& value) = 0;
+    virtual void move(int delta) const = 0;
+
+    virtual ~ITape() = default;
+
+};
